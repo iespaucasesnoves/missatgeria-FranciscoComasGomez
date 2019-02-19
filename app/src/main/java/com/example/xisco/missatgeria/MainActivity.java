483039c30e8c0ca62fa3cef02cd7577214775be6
FranciscoComasGomez <franciscocomas@iespaucasesnoves.cat>
2019-02-19 19:16:18 +0100

    excepcion mainnetworkthreadexcepcion

--- app/src/main/java/com/example/xisco/missatgeria/MainActivity.java
+++ app/src/main/java/com/example/xisco/missatgeria/MainActivity.java
@@ -2,25 +2,10 @@ package com.example.xisco.missatgeria;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
 import java.util.HashMap;
-import java.util.Map;
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity{
     private Button log_btn;
